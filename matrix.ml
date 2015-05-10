b8928b76628f4a3bc89b160590d491c2535041c7
lmdexpr <lmdexpr@gmail.com>
2015-05-10 23:59:34 +0900

    途中経過

--- matrix.ml
+++ matrix.ml
@@ -1,9 +1,21 @@
 open Utils
 
+(*
+ * Matrix is
+ * [| [| a11, a12, a13 .. a1y |],
+ *    [| a21,          .. a2y |],
+ *    .         .
+ *    .           .
+ *    [| ax1,          .. axy |] |]
+ *)
+
 let map = Array.map << Array.map
 
 let add lhs = Array.mapi @@ fun x -> Array.mapi << ( + ) << Array.get lhs.(x)
 let (<+>) = add
+
+let sub lhs = add lhs << map (fun e -> -e)
+let (<->) = sub
 
 let mul lhs = Array.mapi @@ fun x -> Array.mapi << ( * ) << Array.get lhs.(x)
 let (<*>) = mul
@@ -14,9 +26,9 @@ let determinant m = ()
 let det = determinant
 
 let vertical_compose = Array.mapi @@ Array.append << Array.get
-let (<|>) = vertical_compose
+let (<||>) = vertical_compose
 
 let horizontal_compose lhs rhs = Array.append
-let (<->) = horizontal_compose
+let (<-->) = horizontal_compose
 
 let pivoting m = ()
