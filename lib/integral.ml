1613e5ba0ed43fb232359ed1189949e04943aee8
lmdexpr <lmdexpr@gmail.com>
2015-07-03 09:43:13 +0900

    add integral

--- lib/integral.ml
+++ lib/integral.ml
@@ -1,8 +1,27 @@
-open Types
+open Type
+open Utils
 exception Not_implemented
 
-let rectangles
+let x a h i = float i *. h +. float a
+let y f     = Eval.to_val f
 
-let trapezoidal
+let rectangles n f a b =
+  let h = float (b - a) /. float n in
+  let y = y f <~ x a h in 
+  h *. Array.reduce (+.) (Array.init n y)
 
-let simpson
+let trapezoidal n f a b =
+  let h = float (b - a) /. float n in
+  let y = y f <~ x a h in 
+  let h = h /. 2. in
+  let iter i = y i +. y (i+1) in
+  h *. Array.reduce (+.) (Array.init n iter)
+
+let simpson n f a b =
+  let h = float (b - a) /. float n in
+  let y = y f <~ x a h in 
+  let h = h /. 3. in
+  let iter i = y i +. (4. *. y (i+1)) +. y (i+2) in
+  let iter = iter <~ ( * ) 2 in
+  if n mod 2 <> 0 then invalid_arg "simpson's n should be even number."
+  else h *. Array.reduce (+.) (Array.init (n / 2) iter)
