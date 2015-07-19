f80e4288686fc59aa923e97e2f863a0597b4e2d5
lmdexpr <lmdexpr@gmail.com>
2015-07-19 23:38:43 +0900

    fix bugs

--- lib/integral.ml
+++ lib/integral.ml
@@ -10,21 +10,21 @@ let sum = Array.reduce (+.)
 
 let rectangles n f a b =
   let h = h n a b in
-  h *. sum (Array.init n @@ y f a h)
+  h *. sum (Array.init (n-1) @@ y f a h)
 
 let trapezoidal n f a b =
-  let h = h n a b /. 2. in
+  let h = h n a b in
   let y = y f a h in
   let iter i = y i +. y (i+1) in
-  h *. sum (Array.init n iter)
+  h /. 2. *. sum (Array.init (n-1) iter)
 
 let simpson n f a b =
-  let h = h n a b /. 3. in
+  let h = h n a b in
   let y = y f a h in
   let iter i = y i +. (4. *. y (i+1)) +. y (i+2) in
   let iter   = iter <~ ( * ) 2 in
   if n mod 2 <> 0 then invalid_arg "simpson's n should be even number."
-  else h *. sum (Array.init (n / 2) iter)
+  else h /. 3. *. sum (Array.init (n / 2 - 1) iter)
 
 let x = ()
 let y = ()
